0b230b4a575c3179cadc5cae5e6e4acf98ec0c50
darciga <darciga@tingsystems.com>
2023-08-01 20:12:36 -0600

    clear docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,6 @@
 python manage.py migrate                  # Apply database migrations
 # Start Gunicorn processes
 echo Starting server
-if [ "$PROCESS_TYPE" = "server" ]; then
   if [ "$DEBUG" = "1" ]; then
     exec uvicorn api.asgi:application \
       --log-level=info --host=0.0.0.0 --port=8080 \
@@ -11,6 +10,3 @@ if [ "$PROCESS_TYPE" = "server" ]; then
   else
     python manage.py runserver 0.0.0.0:8080
   fi
-elif [ "$PROCESS_TYPE" = "worker" ]; then
-  python manage.py rqworker
-fi
