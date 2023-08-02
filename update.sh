94d98aa107a95e2bff17ac31825e6f52c968e6ac
darciga <darciga@tingsystems.com>
2023-08-01 20:10:12 -0600

    add file for userdata

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 dir="/root/compose"
 echo "Update imagedarcigats/api"
 docker pull darcigats/api
-echo "Update mmcb-main worker"
+echo "Update service"
 cd $dir && /usr/local/bin/docker compose down && /usr/local/bin/docker compose up -d --remove-orphans
 docker image prune -a -f
